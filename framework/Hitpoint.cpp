0ae64fa5edb6e32c6bf0a6f85bbd07ada6fb547e
ItsDefinetlyNotMe <simon.foerster@uni-weimar.de>
2022-06-22 16:26:50 +0200

    changed operator== of Hitpoint

---
+++ framework/Hitpoint.cpp
@@ -0,0 +1,4 @@
+#include "Hitpoint.hpp"
+bool operator==(Hitpoint const& h1, Hitpoint const& h2){
+ return h1.color == h2.color && h1.direction == h2.direction && h1.hit == h2.hit && h1.name == h2.name && h1.point3d == h2.point3d && h1.t == h2.t;
+}
