f9dd738393774c1a6a711cd502d30c9ab9080ee9
Viktor <->
2022-07-06 16:42:47 +0200

    Add Material Struct

---
+++ framework/Material.hpp
@@ -0,0 +1,30 @@
+#ifndef MATERIAL_HPP
+#define MATERIAL_HPP
+
+#include <string>
+#include <iostream>
+
+#include <glm/vec3.hpp>
+
+#include "Color.hpp"
+
+struct Material {
+    std::string name;
+
+    Color ka; // Ambient Color
+    Color kd; // Diffuse Color
+    Color ks; // Specular Color
+
+    float m; // Specular Reflection Constant
+
+    friend std::ostream& operator<<(std::ostream& out, Material const& mat) {
+        out << "Material: " << mat.name << "\n"
+            << " Ambient Color: " << mat.ka
+            << " Diffuse Color: " << mat.kd
+            << " Specular Color: " << mat.ks
+            << " Specular Reflection Constant: " << mat.m;
+        return out;
+    }
+};
+
+#endif // MATERIAL_HPP
