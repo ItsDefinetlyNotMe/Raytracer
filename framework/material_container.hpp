b4f9aefe5a5f74c6bfd3431566d6607bcc34ba83
Viktor <61028178+snailcon@users.noreply.github.com>
2022-07-11 12:02:42 +0200

    fix include errors, move SDFreader.cpp into SDFreader.hpp for simplicity

--- framework/material_container.hpp
+++ framework/material_container.hpp
@@ -6,9 +6,9 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <memory>
 std::shared_ptr<Material> find(std::vector<std::shared_ptr<Material>> const& vec, std::string const& mname);
 std::shared_ptr<Material> find(std::map<std::string, std::shared_ptr<Material>> const& vec, std::string const& mname);
 std::shared_ptr<Material> find(std::set<std::shared_ptr<Material>> const& vec, std::string const& mname);
 bool operator<(std::shared_ptr<Material> const& lhs, std::shared_ptr<Material> const& rhs);
 #endif // !MAT_CONTAINER
-
