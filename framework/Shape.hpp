2c07a5589fd685466aa71ca6f3ed4ab7c551b0dc
ItsDefinetlyNotMe <simon.foerster@uni-weimar.de>
2022-08-03 22:08:35 +0200

    updated Sphere, changed material

--- framework/Shape.hpp
+++ framework/Shape.hpp
@@ -6,13 +6,15 @@
 #include <glm/vec3.hpp>
 #include <cmath>
 #include <string>
-#include "color.hpp"
+#include <memory>
+#include "Material.hpp"
 class Shape {
-private: 
-	std::string name_{""};
-	Color color_{};
+private:
+	std::string name_{ "" };
+	std::shared_ptr<Material> mat_;
 public:
-	Shape(std::string const& s, Color const& c);
+	// Shape(std::string const& s, Color const& c);
+	Shape(std::string const& s, std::shared_ptr<Material> const& m);
 	Shape();
 	virtual ~Shape();
 
@@ -20,10 +22,10 @@ public:
 	virtual float area() const = 0;
 	virtual float volume() const = 0;
 	virtual std::ostream& print(std::ostream& os) const;
-	Color get_color() const;
+	// Color get_color() const;
+	std::shared_ptr<Material> get_material() const;
 	std::string get_name() const;
 	virtual Hitpoint intersect(Ray const& ray) const = 0;
 };
 std::ostream& operator<<(std::ostream& os, Shape const& s);
 #endif // ! SHAPE_HPP
-
