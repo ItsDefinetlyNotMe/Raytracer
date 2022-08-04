5aedb307c471a050c8c980bc55d4216d6aec15cb
ItsDefinetlyNotMe <simon.foerster@uni-weimar.de>
2022-08-04 15:40:23 +0200

    added const& to constructor of Camera

--- framework/Camera.cpp
+++ framework/Camera.cpp
@@ -1,2 +1,2 @@
 #include "Camera.hpp"
-Camera::Camera(std::string name, float fov_x) :name_{ name }, fov_x_{ fov_x }, position_{ glm::vec3{} }, front_{ glm::vec3{0.0f,0.0f,-1.0f} }, up_{ glm::vec3{0.0f,1.0f,0.0f} }{}
+Camera::Camera(std::string const& name, float fov_x) :name_{ name }, fov_x_{ fov_x }, position_{ glm::vec3{} }, front_{ glm::vec3{0.0f,0.0f,-1.0f} }, up_{ glm::vec3{0.0f,1.0f,0.0f} }{}
