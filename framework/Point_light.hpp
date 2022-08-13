5850f6db9b11578297eeb7a8f9d3dae7c9b1a0d1
ItsDefinetlyNotMe <simon.foerster@uni-weimar.de>
2022-08-13 20:18:21 +0200

    changed m value to float

--- framework/Point_light.hpp
+++ framework/Point_light.hpp
@@ -2,14 +2,12 @@
 #define POINT_LIGHT
 #include <glm/vec3.hpp>
 #include "color.hpp"
-class Point_Light {
-	// ist ein struct hier besser angebracht ?
-private:
+struct Point_Light {
+	// ist ein struct hier besser angebracht ? jep ist es
 	std::string name_;
 	glm::vec3 position_;
 	Color color_;
-	glm::vec3 brightness_;
-public:
-	Point_Light(std::string const& n, glm::vec3 const& p,Color const& c,glm::vec3 const& b);
+	float brightness_;
+	//Point_Light(std::string const& n, glm::vec3 const& p,Color const& c,glm::vec3 const& b);
 };
 #endif // !POINT_LIGHT
