326f9763cffdcafab1f64865ef2c6500e8d2dcea
ItsDefinetlyNotMe <simon.foerster@uni-weimar.de>
2022-08-24 21:23:40 +0200

    fixed error in cone where hight wasnt scaling correctly

--- framework/Cone.cpp
+++ framework/Cone.cpp
@@ -1,5 +1,10 @@
 #include "Cone.hpp"
 #include "helper.hpp"
+
+Cone::Cone(std::string const& n, std::shared_ptr<Material> const& m, glm::vec3 const& b, float r, float h) :Shape{n,m},bottom_ { b }, radius_{ r }, height_{ h } {}
+Cone::~Cone() {
+	std::cout << "Destructor Cone";
+};
 float Cone::area() const {
 	return 0;
 }
@@ -12,31 +17,50 @@ std::ostream& Cone::print(std::ostream& os) const {
 	return Shape::print(os) << "position: " << "(" << bottom_.x << ", " << bottom_.y << ", " << bottom_.z << ")" << " radius: " << radius_ << ", height: " << height_;
 }
 
+
+
 Hitpoint Cone::intersect(Ray const& ray) const {
 	Ray obj_ray = { world_to_obj_position(ray.origin), glm::normalize(world_to_obj_direction(ray.direction)) };
 
-	glm::vec2 ray_dir_2d{ obj_ray.direction.x,obj_ray.direction.y };
-	glm::vec2 U{ (bottom_ - obj_ray.origin).x,(bottom_ - obj_ray.origin).y };
-	glm::vec2 U1 = glm::dot(U, ray_dir_2d) * ray_dir_2d;
-	glm::vec2 U2{ U - U1 };
+	Hitpoint point{};
+	float disb = ((bottom_.z - obj_ray.origin.z) / obj_ray.direction.z);//bottom.z = obj_ray-origin.z + disb * obj_ray.direction.z
+	float world_disb = disb * Shape::get_scale();
+	glm::vec3 pb = obj_ray.origin + disb * obj_ray.direction;
 
-	float d = glm::length(U2);
-	if (d > radius_)
-		return Hitpoint{};
-
-	float m = sqrt(radius_ * radius_ - d * d);
-	glm::vec2 p2 = glm::vec2{ obj_ray.origin.x,obj_ray.origin.y } + U1 - m * ray_dir_2d;
+	if (glm::length(pb - bottom_) <= radius_) {
+		if (world_disb >= 0.0f)
+				point =  { true, world_disb ,Shape::get_name(),Shape::get_material(),Shape::obj_to_world_position(pb) ,glm::normalize(Shape::obj_to_world_direction(obj_ray.direction)) };
+	}
 	//
-	float t = (p2.x - obj_ray.origin.x) / ray_dir_2d.x;
-	glm::vec3 point_hit = obj_ray.origin + obj_ray.direction * t;
+	glm::vec3 h_dach{ glm::vec3{0.0f,0.0f,bottom_.z - height_} / abs(bottom_.z - height_) };
+	glm::vec3 w{ obj_ray.origin - glm::vec3{bottom_.x,bottom_.y,bottom_.z + height_} };
+	float m = radius_ * radius_/pow(glm::length((glm::vec3{0.0f,0.0f,height_})),2);
 
-	float r = radius_ - (radius_ / height_) * abs((bottom_.z - point_hit.z));
-	if (point_hit.z >= bottom_.z && point_hit.z <= bottom_.z + height_) {
-		if (glm::length(glm::vec2{ bottom_.x,bottom_.y } - p2) <= r) {
-			glm::vec3 world_point_hit{Shape::obj_to_world_position(point_hit)};
-			return Hitpoint{ true, t ,Shape::get_name(),Shape::get_material(), world_point_hit,glm::normalize(ray.direction) };
+	float a = glm::dot(obj_ray.direction, obj_ray.direction) - m * pow(glm::dot(obj_ray.direction, h_dach), 2) - pow(glm::dot(obj_ray.direction, h_dach), 2);
+	float b = 2 * (glm::dot(obj_ray.direction, w) - m * glm::dot(obj_ray.direction, h_dach) * glm::dot(w, h_dach) - glm::dot(obj_ray.direction, h_dach) * glm::dot(w, h_dach));
+	float c = glm::dot(w, w) - m * pow(glm::dot(w, h_dach), 2) - pow(glm::dot(w, h_dach), 2);
+	
+	if (b * b - 4 * a * c < 0)
+		return Hitpoint{};
+	float t1 = (-b + sqrt(b*b -4 * a * c)) / (2 * a);
+	float t2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+	
+	if (t1 <= t2 && t1 > 0) {
+
+		if (t1 * Shape::get_scale() < point.t) {
+			if ((obj_ray.origin + t1 * obj_ray.direction).z > bottom_.z && (obj_ray.origin + t1 * obj_ray.direction).z <= bottom_.z + height_)
+				point = { true, t1 * Shape::get_scale() ,Shape::get_name(),Shape::get_material(),Shape::obj_to_world_position(obj_ray.origin + t1 * obj_ray.direction) ,glm::normalize(Shape::obj_to_world_direction(obj_ray.direction)) };
 		}
 	}
+	if (t2 <= t1 && t2 > 0) {
+		
+		if (t2 * Shape::get_scale() < point.t) {
+			if ((obj_ray.origin + t2 * obj_ray.direction).z > bottom_.z && (obj_ray.origin + t2 * obj_ray.direction).z <= bottom_.z + height_)
+				point = { true, t2 * Shape::get_scale() ,Shape::get_name(),Shape::get_material(),Shape::obj_to_world_position(obj_ray.origin + t2 * obj_ray.direction) ,glm::normalize(Shape::obj_to_world_direction(obj_ray.direction)) };
+		}
+	}
+
+	return point;
 }
 
 glm::vec3 Cone::normal(glm::vec3 const& point) const {
--- framework/Cylinder.cpp
+++ framework/Cylinder.cpp
@@ -17,55 +17,9 @@ std::ostream& Cylinder::print(std::ostream& os) const {
 	return Shape::print(os) << "position: " << "(" << bottom_.x << ", " << bottom_.y << ", " << bottom_.z << ")" << " radius: " << radius_ << ", height: " << height_;
 }
 
-bool intersect_2d_circle(glm::vec3& x1, Ray ray, float radius) {
-	ray.origin.z = 0;
-	ray.direction.z = 0;
-
-	float a = pow(glm::length(ray.direction), 2);
-	if (a == 0.0f)
-		return false;
-	float b = 2 * (ray.origin.x * ray.direction.x - ray.origin.y * ray.direction.y);
-	float c = pow(glm::length(ray.origin), 2) - radius * radius;
-	float root = b * b - 4 * a * c;
-	if (root < 0.0f)
-		return false;
-	root = sqrt(root);
-	if (root == -root) {
-		x1 = ((-b + root) / 2 * a) * ray.direction + ray.origin;
-		return true;
-	}
-	//float tx1 = ((-b + root) / 2 * a);
-	//float tx2 = ((-b - root) / 2 * a);
-	/*if (abs(tx1) < abs(tx2) && tx1 > 0) {
-		x1 = ((-b + root) / 2 * a) * ray.direction + ray.origin;
-		return true;
-	}
-	else if (tx2 > 0) {
-		x1 = ((-b - root) / 2 * a) * ray.direction + ray.origin;
-		return true;
-	}
-	return false;*/
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	glm::vec3 tx1 = ((-b + root) / 2 * a) * ray.direction + ray.origin;
-	glm::vec3 tx2 = ((-b - root) / 2 * a) * ray.direction + ray.origin;
-	
-	if (glm::length(tx1 - ray.origin) >= glm::length(tx2 - ray.origin)) {
-		x1 = tx2;
-		return true;
-	}
-	x1 = tx2;
-}
-
 Hitpoint Cylinder::intersect(Ray const& ray) const {
 	Ray obj_ray = { world_to_obj_position(ray.origin), glm::normalize(world_to_obj_direction(ray.direction)) };
-	/*
-	glm::vec3 point;
-	if (intersect_2d_circle(point, obj_ray, radius_)) {
-		if (glm::length(point) < radius_)
-			std::cout << point.x << point.y << "\n";
-		return Hitpoint{ true, 12 ,Shape::get_name(),Shape::get_material(), point,glm::normalize(ray.direction) };
-	}
-	return Hitpoint{};*/
+
 	Hitpoint point{};
 	float disb = ((bottom_.z - obj_ray.origin.z) / obj_ray.direction.z);
 	float world_disb = disb * Shape::get_scale();
@@ -107,7 +61,7 @@ Hitpoint Cylinder::intersect(Ray const& ray) const {
 	if (t1 < t2 && t1 >= 0 || t2 < 0)
 		t = t1;
 
-	glm::vec3 point_hit = obj_ray.origin + obj_ray.direction * t1;
+	glm::vec3 point_hit = obj_ray.origin + obj_ray.direction * t;
 	if (point_hit.z <= bottom_.z + height_ && point_hit.z >= bottom_.z){
 		float world_t = t * Shape::get_scale();
 		glm::vec3 world_point_hit{ Shape::obj_to_world_position(obj_ray.origin) + glm::normalize(Shape::obj_to_world_direction(obj_ray.direction * world_t)) };
